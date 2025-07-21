bba433e1413f876c5ac1a375a174df845a7ef27a
Zacky Kurniawan <zackykurniawn@gmail.com>
2025-07-21 12:56:45 +0700

    [add] section testimoni

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const testiSwiper = new Swiper(".testiSwiper", {
+  direction: "horizontal",
+  loop: true,
+
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigasi
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
